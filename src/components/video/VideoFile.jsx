71c082724d4db1a90d21e8bce90ab738043c86a0
karthikgpi1 <karthikgopi024@gmail.com>
2022-10-24 16:36:50 +0530

    responsive

--- src/components/video/VideoFile.jsx
+++ src/components/video/VideoFile.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import ReactPlayer from "react-player";
+// import ReactPlayer from "react-player";
+import video from "../../Assets/video.mp4";
 
 const VideoFile = () => {
   return (
     <div>
-      <ReactPlayer
-        
-        url="https://youtu.be/Oy6hk6Y7VHQ"
-    
-        width={387}
+      <video
+        // url="http://youtu.be/Oy6hk6Y7VHQ"
+        src={video}
+        width={375}
         height={200}
         controls={true}
       />
